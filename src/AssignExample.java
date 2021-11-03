033802dae17ff8ddc4b0cf00b0df001d90a5ff42
Sizertt <93645062+Sizertt@users.noreply.github.com>
2021-11-03 15:02:46 +0300

    AssignExample

---
+++ src/AssignExample.java
@@ -0,0 +1,22 @@
+//Пример присваивания значений переменных
+public class AssignExample {
+    public static void main(String[] args) {
+
+        //Обьявляем две переменные
+        int var1;
+        int var2;
+
+       // Присваиваем им значения
+        var1 = 4096;
+        var2 = var1 / 4;
+
+        // Выводим текущее значения переменных
+        System.out.println("var1 = ”" + var1);
+        System.out.println("var2 = var1 / 4 = " + var2);
+
+    } // main()
+
+} // AssignExample class
+
+
+
